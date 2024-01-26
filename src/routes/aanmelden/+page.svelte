1d684e7f59ecb07b063d211b46ca47bb1da96c2b
Emona Santiago <emonasantiago1207@gmail.com>
2024-01-26 18:48:29 +0100

    aanmelden pagina aangepast

--- src/routes/aanmelden/+page.svelte
+++ src/routes/aanmelden/+page.svelte
@@ -2,10 +2,16 @@
     import Content from "../../lib/components/Content.svelte";
   </script>
   
-  <section id="Aanmelden">
+ <section id="Aanmelden">
+<div class="row">
+  <div class="column">
+    <div class="text">
     <h1>Meld je aan!</h1>
     <h2>Voor de WE 💜 WEB</h2>
     <p>Iedereen is welkom!</p>
+  </div>
+  </div>
+  <div class="column">
     <div class="form">
       <form action="https://formspree.io/f/moqzowdg" method="POST">
         <label for="fname">Naam</label>
@@ -45,31 +51,62 @@
         <input type="submit" value="Submit" />
       </form>
     </div>
-  </section>
-  
+  </div>
+</div>
+</section>
+
   <Content />
   
   <style>
     h1 {
       color: white;
       padding-bottom: 2px;
-      font-size: 30px;
+      font-size: 85px;
     }
   
     h2 {
       color: var(--purple);
       padding-bottom: 4px;
+      font-size: 45px;
     }
   
     p {
       color: white;
+      font-size: 20px;
     }
     #Aanmelden {
-      /* height: 70vh; */
-      margin: 2em;
+      height: 50vh;
+      margin: 5em;
     }
+
+    .text {
+	padding: 0;
+	margin-left: 35%;
+	transform: translateX(-200%);
+	opacity: 0;
+	animation: slide-in-anim 0.5s ease-out forwards;
+    }
+
+    @keyframes slide-in-anim {
+	20% {
+		opacity: 0;
+	}
+	60% {
+		transform: translateX(-45%);
+	}
+	75% {
+		transform: translateX(-52%);
+	}
+	100% {
+		opacity: 1;
+		transform: translateX(-50%);
+	}
+}
+
+
   
     .form {
+      color: white;
       margin-top: 2em;
     }
     input[type="text"],
@@ -99,11 +136,46 @@
       background-color: var(--blue);
       color: white;
     }
-  
-    div {
-      border-radius: 5px;
-      background-color: var(--purple);
-      padding: 20px;
-    }
+
+    * {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 50%;
+  height: 200px; 
+}
+
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+
+  #Aanmelden {
+    height: 60vh;
+    margin: 1em;
+  }
+
+  h1 {
+    font-size: 40px;
+  }
+
+  h2 {
+    font-size: 20px;
+  }
+
+  .text {
+    margin-top: 2em;
+  }
+}
+
   </style>
   
